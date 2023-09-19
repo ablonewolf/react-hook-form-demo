e7b423f5d17e57cfe1d60603b4c809f55f309fad
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-09-19 17:23:00 +0600

    added error handler and validator.

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -3,8 +3,18 @@ import {DevTool} from "@hookform/devtools";
 import {formType} from "../types/formType";
 
 export const UserForm = () => {
+
+    function isValidEmail(email: string): boolean {
+        // Regular expression pattern for validating email addresses
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        // Test the email against the pattern
+        return emailPattern.test(email);
+    }
+
     const form = useForm<formType>();
-    const {register, control, handleSubmit} = form;
+    const {register, control, handleSubmit, formState} = form;
+    const {errors} = formState;
 
     const onSubmit = (data: formType) => {
         console.log("Form submitted: ", data);
@@ -12,14 +22,51 @@ export const UserForm = () => {
     return (
         <div>
             <form className="form-container" onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register("username")}/>
-
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" {...register("email")}/>
-
-                <label htmlFor="course">Course</label>
-                <input type="text" id="course" {...register("course")}/>
+                <div className="form-control">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        {...register("username", {
+                            required: "User name is required."
+                        })}
+                    />
+                    <p className="error">{errors.username?.message}</p>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        {...register("email", {
+                                required: "Email address is required",
+                                pattern: {
+                                    value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                    message: "Invalid emaill format"
+                                },
+                                validate: (fieldValue) => {
+                                    console.log(fieldValue)
+                                    if (! isValidEmail(fieldValue)) {
+                                        return "Enter a valid email address";
+                                    }
+                                }
+                            }
+                        )}
+                    />
+                    <p className="error">{errors.email?.message}</p>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="course">Course</label>
+                    <input
+                        type="text"
+                        id="course"
+                        {...register("course", {
+                                required: "Course name is requried."
+                            }
+                        )}
+                    />
+                    <p className="error">{errors.course?.message}</p>
+                </div>
 
                 <button>Submit</button>
             </form>
