e370e835a1629bc4f9a3e3eb02115839fb229ac0
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-09-19 14:25:03 +0600

    added register to handle each input field state.

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,15 +1,20 @@
-export const UserForm = ()=> {
+import {useForm} from "react-hook-form";
+import {DevTool} from "@hookform/devtools";
+
+export const UserForm = () => {
+    const form = useForm();
+    const {register} = form;
     return (
         <div>
             <form className="form-container">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username"/>
+                <input type="text" id="username" {...register("username")}/>
 
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email"/>
+                <input type="email" id="email" {...register("email")}/>
 
                 <label htmlFor="course">Course</label>
-                <input type="text" id="course" name="course"/>
+                <input type="text" id="course" {...register("course")}/>
 
                 <button>Submit</button>
             </form>
